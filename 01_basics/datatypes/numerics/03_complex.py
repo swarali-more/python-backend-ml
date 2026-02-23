8ca87e0932d6e1b1832fafcdbee7aa3f26ed9457
Swarali <swaralimore3@gmail.com>
2026-02-23 22:46:28 +0530

    added complex datatype example

--- 01_basics/datatypes/numerics/03_complex.py
+++ 01_basics/datatypes/numerics/03_complex.py
@@ -1,14 +1,12 @@
-# Complex Number Practice
+# Complex Data Type Example
 
-c1 = 3 + 4j
-c2 = 1 + 2j
+c1 = 2 + 3j
+c2 = 1 + 4j
 
-print("Complex Number 1:", c1)
-print("Complex Number 2:", c2)
+print("Value of c1:", c1)
+print("Value of c2:", c2)
+print("Type of c1:", type(c1))
 
 print("Addition:", c1 + c2)
 print("Subtraction:", c1 - c2)
 print("Multiplication:", c1 * c2)
-
-print("Real Part of c1:", c1.real)
-print("Imaginary Part of c1:", c1.imag)
